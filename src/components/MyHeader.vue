a56144b5b1bb3899e1a6aad03080eabdee0891a0
Henry Kariuki <henrykariuki17@gmail.com>
2024-06-05 13:52:47 +0300

    Initial commit

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -2,12 +2,12 @@
 import { Menu, Settings, Search } from 'lucide-vue-next';
 </script>
 <template>
-<div class=" w-full h-20 flex justify-between items-center">
+<div class=" w-full h-20 flex justify-between items-center  px-7">
   <div class="section-one w-auto h-14 flex items-center">
-    <div class="w-auto h-6 border border-black mr-12"><Menu/></div>
+    <div class="w-auto h-6  mr-12"><Menu size="19"/></div>
     <div class="w-auto h-6 flex items-center font-semibold text-gray-600">E-Commerce-Dashboard</div>
   </div>
-  <div class="section-two w-auto mr-5 h-14 flex items-center justify-between">
+  <div class=" section-two w-auto mr-5 h-14 flex items-center justify-between">
     <div class="w-auto h-12 flex items-center">
       <input placeholder="Search" type="text" class="p-1 pl-6 w-56 border border-gray-400 rounded-2xl hover:border-indigo-500 duration-75">
     </div>
