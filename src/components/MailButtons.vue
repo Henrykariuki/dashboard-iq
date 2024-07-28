cb0b4e92f0547928262443dff7c3b1f56cd6bee7
Henry Kariuki <henrykariuki17@gmail.com>
2024-07-29 00:37:17 +0300

    Added e-mail component

---
+++ src/components/MailButtons.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+    button: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
+<template>
+    <div>
+        <button :class="`${button.backgroundColor} ${button.textColor} ${button.bgHover}`" 
+        class=" p-2 flex items-center flex-row gap-4 rounded-md">
+            <component :is="button.icon" size="16"/>
+            <p class="font-medium">{{ button.name }}</p>
+            <div class=" text-black py-0.5 px-2.5 rounded-full bg-indigo-50">{{ button.number }}</div>
+        </button>
+    </div>
+</template>
--- src/pages/apps/MailInbox.vue
+++ src/pages/apps/MailInbox.vue
@@ -1,35 +1,46 @@
 <script setup>
-import { Inbox, Star, Ban, Bookmark, Send, Booktext, Trash2 } from 'lucide-vue-next';
+import { Inbox, Star, Ban, Bookmark, Send, BookText, Trash2, RotateCcw, EllipsisVertical, Search } from 'lucide-vue-next';
+import MailButtons from '@/components/MailButtons.vue';
 import { ref } from 'vue';
 
-const mailButtons = ref([
-    {icon: Inbox, name: 'Inbox', number: 8},
-    {icon: Star, name: 'Starred', number: 10},
-    {icon: Ban, name: 'Spam', number: 4},
-    {icon: Bookmark, name: 'Important', number: 4},
-    {icon: Send, name: 'Sent', number: 3},
-    {icon: Booktext, name: 'Archived', number: 2},
-    {icon: Trash2, name: 'Trash', number: 1}
+
+const emailButtons = ref([
+    {icon: Inbox, name: 'Inbox', number: 8, backgroundColor: 'bg-indigo-500', textColor: 'text-white', bgHover: 'hover:bg-indigo-600'},
+    {icon: Star, name: 'Starred', number: 10, backgroundColor: '', textColor: '', bgHover: 'hover:bg-indigo-50'},
+    {icon: Ban, name: 'Spam', number: 4, backgroundColor: '', textColor: '', bgHover: 'hover:bg-indigo-50'},
+    {icon: Bookmark, name: 'Important', number: 4, backgroundColor: '', textColor: '', bgHover: 'hover:bg-indigo-50'},
+    {icon: Send, name: 'Sent', number: 3, backgroundColor: '', textColor: '', bgHover: 'hover:bg-indigo-50'},
+    {icon: BookText, name: 'Archived', number: 2, backgroundColor: '', textColor: '', bgHover: 'hover:bg-indigo-50'},
+    {icon: Trash2, name: 'Trash', number: 1, backgroundColor: '', textColor: '', bgHover: 'hover:bg-indigo-50'}
 ])
 </script>
 <template>
     <div>
-        <div class="border border-black grid grid-cols-4">
-            <div class="border border-black bg-white h-40">
+        <div class="border border-black grid grid-cols-6">
+            <div class="border border-black bg-white px-6 overflow-x-auto">
                 <div>
-                    <button class="text-indigo-500 border border-indigo-500 p-2">
+                    <button class="text-indigo-500 hover:bg-indigo-50 border border-indigo-500 p-2 mb-4">
                         Compose New
                     </button>
                 </div>
-                <div class="">
-                    <button class="border border-black hover:bg-gray-50 p-2 flex items-center flex-row gap-4 rounded-md">
-                        <Star size="16"/>
-                        <p class="font-medium">Starred</p>
-                        <div class=" py-0.5 px-2.5 rounded-full bg-indigo-50">7</div>
-                    </button>
+                <div class="flex flex-col gap-4">
+                    <MailButtons v-for="(info, index) in emailButtons" :key="`id-${index}`" :button="info" />
                 </div>
             </div>
-            <div class="border border-black col-span-3 h-40"></div>
+            <div class="border border-black col-span-5 h-40">
+                <div class="border border-black flex items-center  overflow-x-auto">
+                    <div class="border border-black min-w-80 p-2 flex flex-row gap-8">
+                        <input class="h-5 w-5" type="checkbox">
+                        <div><RotateCcw size="18"/></div>
+                        <div><EllipsisVertical size="18"/></div>
+                    </div>
+                    <div class="border border-black min-w-80"></div>
+                    <div class="relative border border-black min-w-80 flex justify-end">
+                        <input class="border border-black pl-11 py-2" placeholder="Search Mail" type="text">
+                        <button class="absolute left-24 top-2.5 text-gray-500"><Search/></button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
