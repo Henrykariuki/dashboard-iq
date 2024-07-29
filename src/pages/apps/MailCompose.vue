2e8b875447866cbc3fa5bbbc7a0c61f981c23a35
Henry Kariuki <henrykariuki17@gmail.com>
2024-07-29 15:39:59 +0300

    Initial commit

--- src/pages/apps/MailCompose.vue
+++ src/pages/apps/MailCompose.vue
@@ -1,5 +1,17 @@
 <script setup>
+import emailButtons from '@/pages/apps/MailInbox.vue'
+import MailButtons from '@/components/MailButtons.vue';
+import { ref } from 'vue';
+
+const buttonsTwo = ref(emailButtons);
 </script>
 <template>
-    <div>Compose</div>
+    <div>
+        <div class="grid grid-cols-6">
+            <div class="border border-black bg-white h-60">
+                <MailButtons v-for="(info, index) in buttonsTwo" :key="`id-${index}`" />
+            </div>
+            <div class="border border-black bg-white h-60 col-span-5"></div>
+        </div>
+    </div>
 </template>
--- src/pages/apps/MailInbox.vue
+++ src/pages/apps/MailInbox.vue
@@ -13,13 +13,29 @@ const emailButtons = ref([
     {icon: BookText, name: 'Archived', number: 2, backgroundColor: '', textColor: '', bgHover: 'hover:bg-indigo-50'},
     {icon: Trash2, name: 'Trash', number: 1, backgroundColor: '', textColor: '', bgHover: 'hover:bg-indigo-50'}
 ])
+
+
+const emails = ref([
+    {image: '/images/james.jpg', name: 'James Carter', text: 'Eget ipsum quam eu a, sit pellentesque molestie tristique...', date: '16 Jun 2024'},
+    {image: '/images/steve.jpg', name: 'steve Grace', text: 'Consequat sed nibh laoreet ultrices at elit tellus, nunc. Nequ...', date: '16 Jun 2024'},
+    {image: '/images/joram.jpg', name: 'Joram moses', text: 'Consectetur sed dis viverra lorem. Augue felis sed elit rhoncu...', date: '16 Jun 2024'},
+    {image: '/images/james.jpg', name: 'James Carter', text: 'Eget ipsum quam eu a, sit pellentesque molestie tristique...', date: '16 Jun 2024'},
+    {image: '/images/steve.jpg', name: 'steve Grace', text: 'Consequat sed nibh laoreet ultrices at elit tellus, nunc. Nequ...', date: '16 Jun 2024'},
+    {image: '/images/joram.jpg', name: 'Joram moses', text: 'Consectetur sed dis viverra lorem. Augue felis sed elit rhoncu...', date: '16 Jun 2024'},
+    {image: '/images/james.jpg', name: 'James Carter', text: 'Eget ipsum quam eu a, sit pellentesque molestie tristique...', date: '16 Jun 2024'},
+    {image: '/images/steve.jpg', name: 'steve Grace', text: 'Consequat sed nibh laoreet ultrices at elit tellus, nunc. Nequ...', date: '16 Jun 2024'},
+    {image: '/images/joram.jpg', name: 'Joram moses', text: 'Consectetur sed dis viverra lorem. Augue felis sed elit rhoncu...', date: '16 Jun 2024'},
+    
+])
+
+
 </script>
 <template>
-    <div>
-        <div class="border border-black grid grid-cols-6">
-            <div class="border border-black bg-white px-6 overflow-x-auto">
+    <div class="bg-white">
+        <div class=" pt-10  grid grid-cols-6 gap-8">
+            <div class="bg-white px-6 overflow-x-auto">
                 <div>
-                    <button class="text-indigo-500 hover:bg-indigo-50 border border-indigo-500 p-2 mb-4">
+                    <button class="text-indigo-500 rounded-md hover:bg-indigo-50 border border-indigo-500 p-2 mb-4">
                         Compose New
                     </button>
                 </div>
@@ -27,18 +43,38 @@ const emailButtons = ref([
                     <MailButtons v-for="(info, index) in emailButtons" :key="`id-${index}`" :button="info" />
                 </div>
             </div>
-            <div class="border border-black col-span-5 h-40">
-                <div class="border border-black flex items-center  overflow-x-auto">
-                    <div class="border border-black min-w-80 p-2 flex flex-row gap-8">
+            <div class="col-span-5 bg-white overflow-x-auto">
+                <div class=" w-full border-b border-gray-300 py-4  bg-gray-100 flex items-center  ">
+                    <div class=" min-w-80 p-2 flex flex-row gap-5">
                         <input class="h-5 w-5" type="checkbox">
                         <div><RotateCcw size="18"/></div>
                         <div><EllipsisVertical size="18"/></div>
                     </div>
-                    <div class="border border-black min-w-80"></div>
-                    <div class="relative border border-black min-w-80 flex justify-end">
-                        <input class="border border-black pl-11 py-2" placeholder="Search Mail" type="text">
-                        <button class="absolute left-24 top-2.5 text-gray-500"><Search/></button>
+                    <div class=" min-w-72"></div>
+                    <div class="relative min-w-80 flex ">
+                        <input class=" pl-11 py-2 border border-gray-300 transition duration-150 hover:border-indigo-500 rounded-md" placeholder="Search Mail" type="text">
+                        <button class="absolute left-2 top-2.5 text-gray-500"><Search/></button>
                     </div>
+                </div>
+                <div v-for="(info, index) in emails" :key="`id-${index}`"
+                 class="container hover:bg-gray-100  flex flex-row gap-2 jusify-between border-b border-gray-300 py-4 ">
+                    <div class="flex flew-row gap-2   ">
+                        <div class="  p-2 flex items-center flex-row gap-5">
+                            <input class="h-4 w-4" type="checkbox">
+                            <div><Star size="18"/></div>
+                            <div><Bookmark size="18"/></div>
+                        </div>
+                        <div class="flex items-center  ">
+                            <div><img class="w-8 h-8 object-cover rounded-full" :src="info.image"></div>
+                            <p class="font-semibold text-lg ml-2 text-nowrap">{{ info.name }}</p>
+                        </div>
+                    </div>
+                    <div class="  text-nowrap pl-2 w-full font-medium text-gray-700 flex items-center">
+                        <p>{{ info.text }}</p>
+                    </div>
+                    <div class="text-nowrap flex items-center">
+                        <p class="font-medium text-gray-700">{{ info.date }}</p>
+                    </div>  
                 </div>
             </div>
         </div>
